94529fc3fe2758983cc8e731cb7cb4c242cbf10f
René Moser <mail@renemoser.net>
2015-03-11 22:33:28 +0100

    cloudstack_sshkey: refactor to oop

--- cloudstack_sshkey.py
+++ cloudstack_sshkey.py
@@ -98,80 +98,208 @@ EXAMPLES = '''
 - cloudstack_sshkey: name=linus@example.com public_key='{{ lookup('file', '~/.ssh/id_rsa.pub') }}'
 '''
 
+import sys
+
 try:
     from cs import CloudStack, CloudStackException, read_config
 except ImportError:
     print("failed=True " + \
-        "msg='python library cs required. pip install cs'")
+        "msg='python library cs required: pip install cs'")
     sys.exit(1)
 
 
-def get_project_id(module, cs):
-    project = module.params.get('project')
-    if not project:
-        return ''
+class AnsibleCloudStack:
 
-    projects = cs.listProjects()
-    if projects:
-        for p in projects['project']:
-            if p['name'] == project or p['displaytext'] == project or p['id'] == project:
-                return p['id']
-    module.fail_json(msg="project '%s' not found" % project)
+    def __init__(self, module):
+        self.module = module
+        self._connect()
+
+        self.project_id = None
+        self.ip_address_id = None
+        self.zone_id = None
+        self.vm_id = None
+        self.os_type_id = None
 
 
-def register_ssh_key(module, cs, result, ssh_key, project_id):
-    if not ssh_key:
+    def _connect(self):
+        api_key = self.module.params.get('api_key')
+        api_secret = self.module.params.get('secret_key')
+        api_url = self.module.params.get('api_url')
+        api_http_method = self.module.params.get('api_http_method')
+
+        if api_key and api_secret and api_url:
+            self.cs = CloudStack(
+                endpoint=api_url,
+                key=api_key,
+                secret=api_secret,
+                method=api_http_method
+                )
+        else:
+            self.cs = CloudStack(**read_config())
+
+
+    def get_project_id(self):
+        if self.project_id:
+            return self.project_id
+
+        project = self.module.params.get('project')
+        if not project:
+            return None
+
+        projects = self.cs.listProjects()
+        if projects:
+            for p in projects['project']:
+                if project in [ p['name'], p['displaytext'], p['id'] ]:
+                    self.project_id = p[id]
+                    return self.project_id
+        self.module.fail_json(msg="project '%s' not found" % project)
+
+
+    def get_ip_address_id(self):
+        if self.ip_address_id:
+            return self.ip_address_id
+
         args = {}
-        if project_id:
-            args['projectid'] = project_id
+        args['ipaddress'] = self.module.params.get('ip_address')
+        args['projectid'] = self.get_project_id()
+        ip_addresses = self.cs.listPublicIpAddresses(**args)
 
-        result['changed'] = True
-        if not module.check_mode:
-            args['name'] = module.params.get('name')
-            args['publickey'] = module.params.get('public_key')
-            ssh_key = cs.registerSSHKeyPair(**args)
-    return (result, ssh_key)
+        if not ip_addresses:
+            self.module.fail_json(msg="ip address '%s' not found" % args['ipaddress'])
 
-
-def create_ssh_key(module, cs, result, ssh_key, project_id):
-    if not ssh_key:
-        args = {}
-        if project_id:
-            args['projectid'] = project_id
-
-        result['changed'] = True
-        if not module.check_mode:
-            args['name'] = module.params.get('name')
-            res = cs.createSSHKeyPair(**args)
-            ssh_key = res['keypair']
-    return (result, ssh_key)
+        self.ip_address_id = ip_addresses['publicipaddress'][0]['id']
+        return self.ip_address_id
 
 
-def remove_ssh_key(module, cs, result, ssh_key):
-    if ssh_key:
-        result['changed'] = True
-        if not module.check_mode:
+    def get_vm_id(self):
+        if self.vm_id:
+            return self.vm_id
+
+        vm = self.module.params.get('vm')
+        args['projectid'] = self.get_project_id()
+        vms = self.cs.listVirtualMachines(**args)
+        if vms:
+            for v in vms['virtualmachine']:
+                if vm in [ v['name'], v['id'] ]:
+                    self.vm_id = v['id']
+                    return self.vm_id
+        self.module.fail_json(msg="Virtual machine '%s' not found" % vm)
+
+
+    def get_zone_id(self):
+        if self.zone_id:
+            return self.zone_id
+
+        zone = self.module.params.get('zone')
+        zones = self.cs.listZones()
+
+        # use the first zone if no zone param
+        if not zone:
+            self.zone_id = zones['zone'][0]['id']
+            return self.zone_id
+
+        if zones:
+            for z in zones['zone']:
+                if zone in [ z['name'], z['id'] ]:
+                    self.zone_id = z['id']
+                    return self.zone_id
+        self.module.fail_json(msg="zone '%s' not found" % zone)
+
+
+    def get_os_type_id(self):
+        if self.os_type_id:
+            return self.os_type_id
+
+        os_type = self.module.params.get('os_type')
+        if not os_type:
+            return None
+
+        os_types = self.cs.listOsTypes()
+        if os_types:
+            for o in os_types['ostype']:
+                if os_type in [ o['description'], o['id'] ]:
+                    self.os_type_id = o['id']
+                    return self.os_type_id
+        self.module.fail_json(msg="OS type '%s' not found" % os_type)
+
+
+    def _poll_job(self, job, key):
+        if 'jobid' in job:
+            while True:
+                res = self.cs.queryAsyncJobResult(jobid=job['jobid'])
+                if res['jobstatus'] != 0:
+                    if 'jobresult' in res and key in res['jobresult']:
+                        job = res['jobresult'][key]
+                    break
+                time.sleep(2)
+        return job
+
+
+class AnsibleCloudStackSshKey(AnsibleCloudStack):
+
+    def __init__(self, module):
+        AnsibleCloudStack.__init__(self, module)
+
+
+    def register_ssh_key(self, ssh_key):
+        if not ssh_key:
             args = {}
-            args['name'] = module.params.get('name')
-            res = cs.deleteSSHKeyPair(**args)
-    return (result, ssh_key)
+            args['projectid'] = self.get_project_id()
+
+            self.result['changed'] = True
+            if not self.module.check_mode:
+                args['name'] = self.module.params.get('name')
+                args['publickey'] = self.module.params.get('public_key')
+                ssh_key = self.cs.registerSSHKeyPair(**args)
+        return ssh_key
 
 
-def get_ssh_key(module, cs, project_id):
+    def create_ssh_key(self, ssh_key):
+        if not ssh_key:
+            args = {}
+            args['projectid'] = self.get_project_id()
+            
+            self.result['changed'] = True
+            if not self.module.check_mode:
+                args['name'] = self.module.params.get('name')
+                res = self.cs.createSSHKeyPair(**args)
+                ssh_key = res['keypair']
+            return ssh_key
 
-    args = {}
 
-    if project_id:
-        args['projectid'] = project_id
+    def remove_ssh_key(self, ssh_key):
+        if ssh_key:
+            self.result['changed'] = True
+            if not self.module.check_mode:
+                args = {}
+                args['name'] = self.module.params.get('name')
+                res = self.cs.deleteSSHKeyPair(**args)
+        return ssh_key
 
-    args['name'] = module.params.get('name')
 
-    ssh_keys = cs.listSSHKeyPairs(**args)
+    def get_ssh_key(self):
+        args = {}
+        args['projectid'] = self.get_project_id()
+        args['name'] = self.module.params.get('name')
 
-    if ssh_keys:
-        return ssh_keys['sshkeypair'][0]
+        ssh_keys = self.cs.listSSHKeyPairs(**args)
+        if ssh_keys and 'sshkeypair' in ssh_keys:
+            return ssh_keys['sshkeypair'][0]
+        return None
 
-    return None
+
+    def get_result(self, ssh_key):
+        if ssh_key:
+            if 'fingerprint' in ssh_key:
+                self.result['fingerprint'] = ssh_key['fingerprint']
+
+            if 'name' in ssh_key:
+                self.result['name'] = ssh_key['name']
+
+            if 'privatekey' in ssh_key:
+                self.result['private_key'] = ssh_key['privatekey']
+        return self.result
+
 
 def main():
     module = AnsibleModule(
@@ -188,48 +316,22 @@ def main():
         supports_check_mode=True
     )
 
-    result = {}
-    result['changed'] = False
-    state = module.params.get('state')
-
     try:
-        api_key = module.params.get('api_key')
-        api_secret = module.params.get('secret_key')
-        api_url = module.params.get('api_url')
-        api_http_method = module.params.get('api_http_method')
+        ansible_cloudstack_sshkey = AnsibleCloudStackSshKey(module)
+        ssh_key = ansible_cloudstack_sshkey.get_ssh_key()
 
-        if api_key and api_secret and api_url:
-            cs = CloudStack(
-                endpoint=api_url,
-                key=api_key,
-                secret=api_secret,
-                method=api_http_method
-                )
-        else:
-            cs = CloudStack(**read_config())
-
-        project_id = get_project_id(module, cs)
-        ssh_key = get_ssh_key(module, cs, project_id)
-
+        state = module.params.get('state')
         if state in ['absent']:
-            (result, ssh_key) = remove_ssh_key(module, cs, result, ssh_key)
+            ssh_key = ansible_cloudstack_sshkey.remove_ssh_key(ssh_key)
         else:
             if module.params.get('public_key'):
-                (result, ssh_key) = register_ssh_key(module, cs, result, ssh_key, project_id)
+                ssh_key = ansible_cloudstack_sshkey.register_ssh_key(ssh_key)
             else:
-                (result, ssh_key) = create_ssh_key(module, cs, result, ssh_key, project_id)
+                ssh_key = ansible_cloudstack_sshkey.create_ssh_key(ssh_key)
+
+        result = ansible_cloudstack_sshkey.get_result(ssh_key)
     except CloudStackException, e:
         module.fail_json(msg='CloudStackException: %s' % str(e))
-
-    if ssh_key:
-        if 'fingerprint' in ssh_key:
-            result['fingerprint'] = ssh_key['fingerprint']
-
-        if 'name' in ssh_key:
-            result['name'] = ssh_key['name']
-
-        if 'privatekey' in ssh_key:
-            result['private_key'] = ssh_key['privatekey']
 
     module.exit_json(**result)
 
