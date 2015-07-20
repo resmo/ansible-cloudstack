62f7e7bc32e2d0a45f96f77e2ed9e3778f85fb1e
Rene Moser <mail@renemoser.net>
2015-07-20 23:46:00 +0200

    sync from ansible-modules-extras

--- cs_project.py
+++ cs_project.py
@@ -478,13 +478,6 @@ class AnsibleCloudStackProject(AnsibleCloudStack):
         self.project = None
 
 
-    def get_displaytext(self):
-        displaytext = self.module.params.get('displaytext')
-        if not displaytext:
-            displaytext = self.module.params.get('name')
-        return displaytext
-
-
     def get_project(self):
         if not self.project:
             project = self.module.params.get('name')
@@ -514,7 +507,7 @@ class AnsibleCloudStackProject(AnsibleCloudStack):
     def update_project(self, project):
         args                = {}
         args['id']          = project['id']
-        args['displaytext'] = self.get_displaytext()
+        args['displaytext'] = self.get_or_fallback('displaytext', 'name')
 
         if self._has_changed(args, project):
             self.result['changed'] = True
@@ -535,7 +528,7 @@ class AnsibleCloudStackProject(AnsibleCloudStack):
 
         args                = {}
         args['name']        = self.module.params.get('name')
-        args['displaytext'] = self.get_displaytext()
+        args['displaytext'] = self.get_or_fallback('displaytext', 'name')
         args['account']     = self.get_account('name')
         args['domainid']    = self.get_domain('id')
 
--- cs_staticnat.py
+++ cs_staticnat.py
@@ -20,79 +20,47 @@
 
 DOCUMENTATION = '''
 ---
-module: cs_portforward
-short_description: Manages port forwarding rules on Apache CloudStack based clouds.
+module: cs_staticnat
+short_description: Manages static NATs on Apache CloudStack based clouds.
 description:
-    - Create, update and remove port forwarding rules.
+    - Create, update and remove static NATs.
 version_added: '2.0'
 author: "René Moser (@resmo)"
 options:
   ip_address:
     description:
-      - Public IP address the rule is assigned to.
+      - Public IP address the static NAT is assigned to.
     required: true
   vm:
     description:
-      - Name of virtual machine which we make the port forwarding rule for.
+      - Name of virtual machine which we make the static NAT for.
       - Required if C(state=present).
     required: false
     default: null
+  vm_guest_ip:
+    description:
+      - VM guest NIC secondary IP address for the static NAT.
+    required: false
+    default: false
   state:
     description:
-      - State of the port forwarding rule.
+      - State of the static NAT.
     required: false
     default: 'present'
     choices: [ 'present', 'absent' ]
-  protocol:
-    description:
-      - Protocol of the port forwarding rule.
-    required: false
-    default: 'tcp'
-    choices: [ 'tcp', 'udp' ]
-  public_port:
-    description:
-      - Start public port for this rule.
-    required: true
-  public_end_port:
-    description:
-      - End public port for this rule.
-      - If not specified equal C(public_port).
-    required: false
-    default: null
-  private_port:
-    description:
-      - Start private port for this rule.
-    required: true
-  private_end_port:
-    description:
-      - End private port for this rule.
-      - If not specified equal C(private_port).
-    required: false
-    default: null
-  open_firewall:
-    description:
-      - Whether the firewall rule for public port should be created, while creating the new rule.
-      - Use M(cs_firewall) for managing firewall rules.
-    required: false
-    default: false
-  vm_guest_ip:
-    description:
-      - VM guest NIC secondary IP address for the port forwarding rule.
-    required: false
-    default: false
   domain:
     description:
-      - Domain the C(vm) is related to.
+      - Domain the static NAT is related to.
     required: false
     default: null
   account:
     description:
-      - Account the C(vm) is related to.
+      - Account the static NAT is related to.
     required: false
     default: null
   project:
     description:
-      - Name of the project the C(vm) is located in.
+      - Name of the project the static NAT is related to.
     required: false
     default: null
   zone:
@@ -110,39 +78,16 @@ extends_documentation_fragment: cloudstack
 '''
 
 EXAMPLES = '''
-# 1.2.3.4:80 -> web01:8080
+# create a static NAT: 1.2.3.4 -> web01
 - local_action:
-    module: cs_portforward
+    module: cs_staticnat
     ip_address: 1.2.3.4
     vm: web01
-    public_port: 80
-    private_port: 8080
 
-# forward SSH and open firewall
+# remove a static NAT
 - local_action:
-    module: cs_portforward
-    ip_address: '{{ public_ip }}'
-    vm: '{{ inventory_hostname }}'
-    public_port: '{{ ansible_ssh_port }}'
-    private_port: 22
-    open_firewall: true
-
-# forward DNS traffic, but do not open firewall
-- local_action:
-    module: cs_portforward
+    module: cs_staticnat
     ip_address: 1.2.3.4
-    vm: '{{ inventory_hostname }}'
-    public_port: 53
-    private_port: 53
-    protocol: udp
-    open_firewall: true
-
-# remove ssh port forwarding
-- local_action:
-    module: cs_portforward
-    ip_address: 1.2.3.4
-    public_port: 22
-    private_port: 22
     state: absent
 '''
 
@@ -153,35 +98,6 @@ ip_address:
   returned: success
   type: string
   sample: 1.2.3.4
-protocol:
-  description: Protocol.
-  returned: success
-  type: string
-  sample: tcp
-private_port:
-  description: Start port on the virtual machine's IP address.
-  returned: success
-  type: int
-  sample: 80
-private_end_port:
-  description: End port on the virtual machine's IP address.
-  returned: success
-  type: int
-public_port:
-  description: Start port on the public IP address.
-  returned: success
-  type: int
-  sample: 80
-public_end_port:
-  description: End port on the public IP address.
-  returned: success
-  type: int
-  sample: 80
-tags:
-  description: Tags related to the port forwarding.
-  returned: success
-  type: list
-  sample: []
 vm_name:
   description: Name of the virtual machine.
   returned: success
@@ -197,7 +113,28 @@ vm_guest_ip:
   returned: success
   type: string
   sample: 10.101.65.152
+zone:
+  description: Name of zone the static NAT is related to.
+  returned: success
+  type: string
+  sample: ch-gva-2
+project:
+  description: Name of project the static NAT is related to.
+  returned: success
+  type: string
+  sample: Production
+account:
+  description: Account the static NAT is related to.
+  returned: success
+  type: string
+  sample: example account
+domain:
+  description: Domain the static NAT is related to.
+  returned: success
+  type: string
+  sample: example domain
 '''
+
 
 try:
     from cs import CloudStack, CloudStackException, read_config
@@ -539,14 +476,14 @@ class AnsibleCloudStack:
         return job
 
 
-class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
+class AnsibleCloudStackStaticNat(AnsibleCloudStack):
 
     def __init__(self, module):
         AnsibleCloudStack.__init__(self, module)
-        self.portforwarding_rule = None
         self.vm_default_nic = None
 
 
+# TODO: move it to cloudstack utils, also used in cs_portforward
     def get_vm_guest_ip(self):
         vm_guest_ip = self.module.params.get('vm_guest_ip')
         default_nic = self.get_vm_default_nic()
@@ -560,6 +497,7 @@ class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
         self.module.fail_json(msg="Secondary IP '%s' not assigned to VM" % vm_guest_ip)
 
 
+# TODO: move it to cloudstack utils, also used in cs_portforward
     def get_vm_default_nic(self):
         if self.vm_default_nic:
             return self.vm_default_nic
@@ -573,129 +511,89 @@ class AnsibleCloudStackPortforwarding(AnsibleCloudStack):
         self.module.fail_json(msg="No default IP address of VM '%s' found" % self.module.params.get('vm'))
 
 
-    def get_portforwarding_rule(self):
-        if not self.portforwarding_rule:
-            protocol            = self.module.params.get('protocol')
-            public_port         = self.module.params.get('public_port')
-            public_end_port     = self.get_or_fallback('public_end_port', 'public_port')
-            private_port        = self.module.params.get('private_port')
-            private_end_port    = self.get_or_fallback('private_end_port', 'private_port')
-
-            args = {}
-            args['ipaddressid'] = self.get_ip_address(key='id')
-            args['projectid'] = self.get_project(key='id')
-            portforwarding_rules = self.cs.listPortForwardingRules(**args)
-
-            if portforwarding_rules and 'portforwardingrule' in portforwarding_rules:
-                for rule in portforwarding_rules['portforwardingrule']:
-                    if protocol == rule['protocol'] \
-                        and public_port == int(rule['publicport']):
-                        self.portforwarding_rule = rule
-                        break
-        return self.portforwarding_rule
-
-
-    def present_portforwarding_rule(self):
-        portforwarding_rule = self.get_portforwarding_rule()
-        if portforwarding_rule:
-            portforwarding_rule = self.update_portforwarding_rule(portforwarding_rule)
-        else:
-            portforwarding_rule = self.create_portforwarding_rule()
-        return portforwarding_rule
-
-
-    def create_portforwarding_rule(self):
-        args = {}
-        args['protocol']            = self.module.params.get('protocol')
-        args['publicport']          = self.module.params.get('public_port')
-        args['publicendport']       = self.get_or_fallback('public_end_port', 'public_port')
-        args['privateport']         = self.module.params.get('private_port')
-        args['privateendport']      = self.get_or_fallback('private_end_port', 'private_port')
-        args['openfirewall']        = self.module.params.get('open_firewall')
-        args['vmguestip']           = self.get_vm_guest_ip()
-        args['ipaddressid']         = self.get_ip_address(key='id')
-        args['virtualmachineid']    = self.get_vm(key='id')
-
-        portforwarding_rule = None
+    def create_static_nat(self, ip_address):
         self.result['changed'] = True
-        if not self.module.check_mode:
-            portforwarding_rule = self.cs.createPortForwardingRule(**args)
-            poll_async = self.module.params.get('poll_async')
-            if poll_async:
-                portforwarding_rule = self._poll_job(portforwarding_rule, 'portforwardingrule')
-        return portforwarding_rule
-
-
-    def update_portforwarding_rule(self, portforwarding_rule):
         args = {}
-        args['protocol']            = self.module.params.get('protocol')
-        args['publicport']          = self.module.params.get('public_port')
-        args['publicendport']       = self.get_or_fallback('public_end_port', 'public_port')
-        args['privateport']         = self.module.params.get('private_port')
-        args['privateendport']      = self.get_or_fallback('private_end_port', 'private_port')
-        args['openfirewall']        = self.module.params.get('open_firewall')
-        args['vmguestip']           = self.get_vm_guest_ip()
-        args['ipaddressid']         = self.get_ip_address(key='id')
-        args['virtualmachineid']    = self.get_vm(key='id')
+        args['virtualmachineid'] = self.get_vm(key='id')
+        args['ipaddressid'] = ip_address['id']
+        args['vmguestip'] = self.get_vm_guest_ip()
+        if not self.module.check_mode:
+            res = self.cs.enableStaticNat(**args)
+            if 'errortext' in res:
+                self.module.fail_json(msg="Failed: '%s'" % res['errortext'])
 
-        if self._has_changed(args, portforwarding_rule):
+            # reset ip address and query new values
+            self.ip_address = None
+            ip_address = self.get_ip_address()
+        return ip_address
+
+
+    def update_static_nat(self, ip_address):
+        args = {}
+        args['virtualmachineid'] = self.get_vm(key='id')
+        args['ipaddressid'] = ip_address['id']
+        args['vmguestip'] = self.get_vm_guest_ip()
+
+        # make an alias, so we can use _has_changed()
+        ip_address['vmguestip'] = ip_address['vmipaddress']
+        if self._has_changed(args, ip_address):
             self.result['changed'] = True
             if not self.module.check_mode:
-                # API broken in 4.2.1?, workaround using remove/create instead of update
-                # portforwarding_rule = self.cs.updatePortForwardingRule(**args)
-                self.absent_portforwarding_rule()
-                portforwarding_rule = self.cs.createPortForwardingRule(**args)
-                poll_async = self.module.params.get('poll_async')
-                if poll_async:
-                    portforwarding_rule = self._poll_job(portforwarding_rule, 'portforwardingrule')
-        return portforwarding_rule
+                res = self.cs.disableStaticNat(ipaddressid=ip_address['id'])
+                if 'errortext' in res:
+                    self.module.fail_json(msg="Failed: '%s'" % res['errortext'])
+                res = self._poll_job(res, 'staticnat')
+                res = self.cs.enableStaticNat(**args)
+                if 'errortext' in res:
+                    self.module.fail_json(msg="Failed: '%s'" % res['errortext'])
+
+                # reset ip address and query new values
+                self.ip_address = None
+                ip_address = self.get_ip_address()
+        return ip_address
 
 
-    def absent_portforwarding_rule(self):
-        portforwarding_rule = self.get_portforwarding_rule()
+    def present_static_nat(self):
+        ip_address = self.get_ip_address()
+        if not ip_address['isstaticnat']:
+            ip_address = self.create_static_nat(ip_address)
+        else:
+            ip_address = self.update_static_nat(ip_address)
+        return ip_address
 
-        if portforwarding_rule:
+
+    def absent_static_nat(self):
+        ip_address = self.get_ip_address()
+        if ip_address['isstaticnat']:
             self.result['changed'] = True
-            args = {}
-            args['id'] = portforwarding_rule['id']
-
             if not self.module.check_mode:
-                res = self.cs.deletePortForwardingRule(**args)
+                res = self.cs.disableStaticNat(ipaddressid=ip_address['id'])
+                if 'errortext' in res:
+                    self.module.fail_json(msg="Failed: '%s'" % res['errortext'])
                 poll_async = self.module.params.get('poll_async')
                 if poll_async:
-                    self._poll_job(res, 'portforwardingrule')
-        return portforwarding_rule
+                    res = self._poll_job(res, 'staticnat')
+        return ip_address
 
 
-    def get_result(self, portforwarding_rule):
-        if portforwarding_rule:
-            if 'id' in portforwarding_rule:
-                self.result['id'] = portforwarding_rule['id']
-            if 'virtualmachinedisplayname' in portforwarding_rule:
-                self.result['vm_display_name'] = portforwarding_rule['virtualmachinedisplayname']
-            if 'virtualmachinename' in portforwarding_rule:
-                self.result['vm_name'] = portforwarding_rule['virtualmachinename']
-            if 'ipaddress' in portforwarding_rule:
-                self.result['ip_address'] = portforwarding_rule['ipaddress']
-            if 'vmguestip' in portforwarding_rule:
-                self.result['vm_guest_ip'] = portforwarding_rule['vmguestip']
-            if 'publicport' in portforwarding_rule:
-                self.result['public_port'] = int(portforwarding_rule['publicport'])
-            if 'publicendport' in portforwarding_rule:
-                self.result['public_end_port'] = int(portforwarding_rule['publicendport'])
-            if 'privateport' in portforwarding_rule:
-                self.result['private_port'] = int(portforwarding_rule['privateport'])
-            if 'privateendport' in portforwarding_rule:
-                self.result['private_end_port'] = int(portforwarding_rule['privateendport'])
-            if 'protocol' in portforwarding_rule:
-                self.result['protocol'] = portforwarding_rule['protocol']
-            if 'tags' in portforwarding_rule:
-                self.result['tags'] = []
-                for tag in portforwarding_rule['tags']:
-                    result_tag          = {}
-                    result_tag['key']   = tag['key']
-                    result_tag['value'] = tag['value']
-                    self.result['tags'].append(result_tag)
+    def get_result(self, ip_address):
+        if ip_address:
+            if 'zonename' in ip_address:
+                self.result['zone'] = ip_address['zonename']
+            if 'domain' in ip_address:
+                self.result['domain'] = ip_address['domain']
+            if 'account' in ip_address:
+                self.result['account'] = ip_address['account']
+            if 'project' in ip_address:
+                self.result['project'] = ip_address['project']
+            if 'virtualmachinedisplayname' in ip_address:
+                self.result['vm_display_name'] = ip_address['virtualmachinedisplayname']
+            if 'virtualmachinename' in ip_address:
+                self.result['vm'] = ip_address['virtualmachinename']
+            if 'vmipaddress' in ip_address:
+                self.result['vm_guest_ip'] = ip_address['vmipaddress']
+            if 'ipaddress' in ip_address:
+                self.result['ip_address'] = ip_address['ipaddress']
         return self.result
 
 
@@ -703,15 +601,9 @@ def main():
     module = AnsibleModule(
         argument_spec = dict(
             ip_address = dict(required=True),
-            protocol= dict(choices=['tcp', 'udp'], default='tcp'),
-            public_port = dict(type='int', required=True),
-            public_end_port = dict(type='int', default=None),
-            private_port = dict(type='int', required=True),
-            private_end_port = dict(type='int', default=None),
-            state = dict(choices=['present', 'absent'], default='present'),
-            open_firewall = dict(choices=BOOLEANS, default=False),
-            vm_guest_ip = dict(default=None),
             vm = dict(default=None),
+            vm_guest_ip = dict(default=None),
+            state = dict(choices=['present', 'absent'], default='present'),
             zone = dict(default=None),
             domain = dict(default=None),
             account = dict(default=None),
@@ -733,14 +625,15 @@ def main():
         module.fail_json(msg="python library cs required: pip install cs")
 
     try:
-        acs_pf = AnsibleCloudStackPortforwarding(module)
+        acs_static_nat = AnsibleCloudStackStaticNat(module)
+
         state = module.params.get('state')
         if state in ['absent']:
-            pf_rule = acs_pf.absent_portforwarding_rule()
+            ip_address = acs_static_nat.absent_static_nat()
         else:
-            pf_rule = acs_pf.present_portforwarding_rule()
+            ip_address = acs_static_nat.present_static_nat()
 
-        result = acs_pf.get_result(pf_rule)
+        result = acs_static_nat.get_result(ip_address)
 
     except CloudStackException, e:
         module.fail_json(msg='CloudStackException: %s' % str(e))
