76edb5263a572134c3f08468119cc79c3484aa6a
Rene Moser <mail@renemoser.net>
2015-10-17 10:42:54 +0200

    sync utils with upstream

--- ansible_cloudstack_utils.py
+++ ansible_cloudstack_utils.py
@@ -1,4 +1,18 @@
+def cs_argument_spec():
+    return dict(
+        api_key = dict(default=None),
+        api_secret = dict(default=None, no_log=True),
+        api_url = dict(default=None),
+        api_http_method = dict(choices=['get', 'post'], default='get'),
+        api_timeout = dict(type='int', default=10),
+        api_region = dict(default='cloudstack'),
+    )
+
+def cs_required_together():
+    return [['api_key', 'api_secret', 'api_url']]
+
 class AnsibleCloudStack(object):
+
     def __init__(self, module):
         if not has_lib_cs:
             module.fail_json(msg="python library cs required: pip install cs")
@@ -25,6 +39,8 @@ class AnsibleCloudStack(object):
 
         # Init returns dict for use in subclasses
         self.returns = {}
+        # these values will be casted to int
+        self.returns_to_int = {}
 
         self.module = module
         self._connect()
@@ -38,6 +54,7 @@ class AnsibleCloudStack(object):
         self.os_type = None
         self.hypervisor = None
         self.capabilities = None
+        self.tags = None
 
 
     def _connect(self):
@@ -267,22 +284,23 @@ class AnsibleCloudStack(object):
 
 
     def get_tags(self, resource=None):
-        args = {}
-        args['projectid'] = self.get_project(key='id')
-        args['domainid'] = self.get_domain(key='id')
-        args['resourceid'] = resource['id']
-
-        response = self.cs.listTags(**args)
+        if not self.tags:
+            args = {}
+            args['projectid'] = self.get_project(key='id')
+            args['account'] = self.get_account(key='name')
+            args['domainid'] = self.get_domain(key='id')
+            args['resourceid'] = resource['id']
+            response = self.cs.listTags(**args)
+            self.tags = response.get('tag', [])
 
         existing_tags = []
-
-        if response:
-            for tag in response['tag']:
+        if self.tags:
+            for tag in self.tags:
                 existing_tags.append({'key': tag['key'], 'value': tag['value']})
-
         return existing_tags
 
-    def _delete_tags(self, resource, resource_type, tags):
+
+    def _process_tags(self, resource, resource_type, tags, operation="create"):
         if tags:
             self.result['changed'] = True
             if not self.module.check_mode:
@@ -290,26 +308,21 @@ class AnsibleCloudStack(object):
                 args['resourceids']  = resource['id']
                 args['resourcetype'] = resource_type
                 args['tags']         = tags
-                self.cs.deleteTags(**args)
+                if operation == "create":
+                    self.cs.createTags(**args)
+                else:
+                    self.cs.deleteTags(**args)
 
-
-    def _create_tags(self, resource, resource_type, tags):
-        if tags:
-            self.result['changed'] = True
-            if not self.module.check_mode:
-                args = {}
-                args['resourceids']  = resource['id']
-                args['resourcetype'] = resource_type
-                args['tags']         = tags
-                self.cs.createTags(**args)
 
     def _tags_that_should_exist_or_be_updated(self, resource, tags):
         existing_tags = self.get_tags(resource)
         return [tag for tag in tags if tag not in existing_tags]
 
+
     def _tags_that_should_not_exist(self, resource, tags):
         existing_tags = self.get_tags(resource)
         return [tag for tag in existing_tags if tag not in tags]
+
 
     def ensure_tags(self, resource, resource_type=None):
         if not resource_type or not resource:
@@ -318,24 +331,11 @@ class AnsibleCloudStack(object):
         if 'tags' in resource:
             tags = self.module.params.get('tags')
             if tags is not None:
-                self._delete_tags(resource, resource_type, self._tags_that_should_not_exist(resource, tags))
-                self._create_tags(resource, resource_type, self._tags_that_should_exist_or_be_updated(resource, tags))
+                self._process_tags(resource, resource_type, self._tags_that_should_exist_or_be_updated(resource, tags))
+                self._process_tags(resource, resource_type, self._tags_that_should_not_exist(resource, tags), operation="delete")
+                self.tags = None
                 resource['tags'] = self.get_tags(resource)
         return resource
-
-
-    def get_disk_offering(self, key=None):
-        disk_offering = self.module.params.get('disk_offering')
-
-        if not disk_offering:
-            return None
-
-        disk_offerings = self.cs.listDiskOfferings()
-        if disk_offerings:
-            for d in disk_offerings['diskoffering']:
-                if disk_offering in [d['displaytext'], d['name'], d['id']]:
-                    return self._get_by_key(key, d)
-        self.module.fail_json(msg="Disk offering '%s' not found" % disk_offering)
 
 
     def get_capabilities(self, key=None):
@@ -346,7 +346,7 @@ class AnsibleCloudStack(object):
         return self._get_by_key(key, self.capabilities)
 
 
-    # TODO: for backward compatibility only, remove it if not used anymore
+    # TODO: for backward compatibility only, remove if not used anymore
     def _poll_job(self, job=None, key=None):
         return self.poll_job(job=job, key=key)
 
@@ -372,6 +372,11 @@ class AnsibleCloudStack(object):
             for search_key, return_key in returns.iteritems():
                 if search_key in resource:
                     self.result[return_key] = resource[search_key]
+
+            # Bad bad API does not always return int when it should.
+            for search_key, return_key in self.returns_to_int.iteritems():
+                if search_key in resource:
+                    self.result[return_key] = int(resource[search_key])
 
             # Special handling for tags
             if 'tags' in resource:
